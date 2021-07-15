592654c111327bea6c69e416cf4b341bb304886b
ilijapet <ilijapet@gmail.com>
2021-07-15 21:19:46 +0200

    mhmm2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='public',
-    version='0.0.1',
+    version='0.0.0',
     author='Ilija Petroniejvic',
     author_email='ilijapet@gmail.com',
     description='Testing installation of Package',
