40896eb75c0afa123a0864521450ee49d493471f
ilijapet <ilijapet@gmail.com>
2021-07-15 20:19:16 +0200

    last, but not liest....

--- paket/setup.py
+++ paket/setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='fun',
+    name='paket',
     version='0.0.1',
     author='Ilija Petroniejvic',
     author_email='ilijapet@gmail.com',
     description='Testing installation of Package',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/mark down",
     url='https://github.com/ilijapet/public/tree/Master/paket',
     project_urls = {
         "Bug Tracker": "https://github.com/ilijapet/public/tree/Master/paket/issues"
     },
     license='MIT',
-    packages=['fun'],
+    packages=['paket'],
     install_requires=['requests'],
 )
